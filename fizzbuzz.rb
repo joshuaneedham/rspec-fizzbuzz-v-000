370b76db3d8b6d13c0ea22392a8e28c48f0d9caa
Joshua Needham <joshuabneedham@gmail.com>
2017-09-30 19:29:03 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,20 @@
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 15 == 0
+    "FizzBuzz"
+  else
+    nil
+  end
+end
+#   #Return "Buzz" when the number is divisible by 5
+# elsif int % 5 == 0
+#     puts "Buzz"
+#   #Return "FizzBuzz" when the number is divisible by 3 and 5
+# elsif int % 3 && int % 5 == 0
+#     puts "FizzBuzz"
+#   #Return nil if the number is not divisible by 3 or 5
+#   else
+#     puts nil
